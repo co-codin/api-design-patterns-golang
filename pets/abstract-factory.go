ec1ed7406f1874169feb79622606c4f5e6f2b6bb
elijah <cuiyeqing960904@gmail.com>
2024-08-27 01:39:43 +0300

    abstract factory pattern for generating more data

---
+++ pets/abstract-factory.go
@@ -0,0 +1,27 @@
+package pets
+
+import (
+	"api-design-patterns/models"
+	"fmt"
+)
+
+type AnimalInterface interface {
+	Show() string
+} 
+
+type DogFromFactory struct {
+	Pet *models.Dog
+}
+
+func (dff *DogFromFactory) Show() string {
+	return fmt.Sprintf("This animal is a %s", dff.Pet.Breed.Breed)
+}
+
+type CatFromFactory struct {
+	Pet *models.Cat
+}
+
+func (cff *CatFromFactory) Show() string {
+	return fmt.Sprintf("This animal is a %s", cff.Pet.Breed.Breed)
+}
+
