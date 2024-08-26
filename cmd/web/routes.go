b297d744733939b23382950cce0a641a2242cf18
elijah <cuiyeqing960904@gmail.com>
2024-08-27 01:27:13 +0300

    setup some frontend

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,11 @@ func (app *application) routes() http.Handler {
 
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
+	mux.Get("/test-patterns", app.TestPatterns)
+
+	mux.Get("/api/dog-from-factory", app.CreateDogFromFactory)
+	mux.Get("/api/cat-from-factory", app.CreateCatFromFactory)
+
 	mux.Get("/", app.ShowHome)
 	mux.Get("/{page}", app.ShowPage)
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module api-design-patterns
 
 go 1.22.2
 
-require github.com/go-chi/chi/v5 v5.1.0 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/tsawler/toolbox v1.3.1 // indirect
+)
