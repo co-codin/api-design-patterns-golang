ed62afdb5bd1f4555ff5b754882dc75b3fc5f103
elijah <cuiyeqing960904@gmail.com>
2024-09-23 21:57:01 +0300

    mysql repo

--- models/repository.go
+++ models/repository.go
@@ -1,1 +1,17 @@
 package models
+
+import "database/sql"
+
+type Repository interface {
+	AllDogBreeds() ([]*DogBreed, error)
+}
+
+type mysqlRepository struct {
+	DB *sql.DB
+}
+
+func newMySQLRepository(conn *sql.DB) Repository {
+	return &mysqlRepository{
+		DB: conn,
+	}
+}
